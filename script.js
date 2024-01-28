a61c8c9dba8363ac4b1961784326653892a43789
Puranasree M S <129767298+Puranasree@users.noreply.github.com>
2024-01-28 14:35:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let currentPlayer = 'X'; // Player X always starts
-let gameBoard = ['', '', '', '', '', '', '', '', '']; // 3x3 game board
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', '']; 
 let gameActive = true;
-
 function handlePlayerTurn(clickedCellIndex) {
   if (gameBoard[clickedCellIndex] !== '' || !gameActive) {
       return;
@@ -10,7 +9,6 @@ function handlePlayerTurn(clickedCellIndex) {
   checkForWinOrDraw();
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 }
-
 function cellClicked(clickedCellEvent) {
   const clickedCell = clickedCellEvent.target;
   const clickedCellIndex = parseInt(clickedCell.id.replace('cell-', '')) - 1;
@@ -20,43 +18,35 @@ function cellClicked(clickedCellEvent) {
   handlePlayerTurn(clickedCellIndex);
   updateUI();
 }
-
 const cells = document.querySelectorAll('.cell');
-
 cells.forEach(cell => {
   cell.addEventListener('click', cellClicked, false);
 });
-
 function updateUI() {
   for (let i = 0; i < cells.length; i++) {
       cells[i].innerText = gameBoard[i];
   }
 }
-
 function announceWinner(player) {
   const messageElement = document.getElementById('gameMessage');
   messageElement.innerText = `Player ${player} Wins!`;
 }
-
 function announceDraw() {
   const messageElement = document.getElementById('gameMessage');
   messageElement.innerText = 'Game Draw!';
 }
-
 const winConditions = [
-  [0, 1, 2], // Top row
-  [3, 4, 5], // Middle row
-  [6, 7, 8], // Bottom row
-  [0, 3, 6], // Left column
-  [1, 4, 7], // Middle column
-  [2, 5, 8], // Right column
-  [0, 4, 8], // Left-to-right diagonal
-  [2, 4, 6]  // Right-to-left diagonal
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6] 
 ];
-
 function checkForWinOrDraw() {
   let roundWon = false;
-
   for (let i = 0; i < winConditions.length; i++) {
       const [a, b, c] = winConditions[i];
       if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
@@ -64,13 +54,11 @@ function checkForWinOrDraw() {
           break;
       }
   }
-
   if (roundWon) {
       announceWinner(currentPlayer);
       gameActive = false;
       return;
   }
-
   let roundDraw = !gameBoard.includes('');
   if (roundDraw) {
       announceDraw();
@@ -78,7 +66,6 @@ function checkForWinOrDraw() {
       return;
   }
 }
-
 function resetGame() {
   gameBoard = ['', '', '', '', '', '', '', '', ''];
   gameActive = true;
@@ -88,6 +75,5 @@ function resetGame() {
   });
   document.getElementById('gameMessage').innerText = '';
 }
-
 const resetButton = document.getElementById('resetButton');
 resetButton.addEventListener('click', resetGame, false);
